faccdee15994eb9295bb1e3e859c9adfd5ec4d94
rikkaxie <619640227@qq.com>
2020-07-21 20:28:20 +0800

    offer03题

---
+++ sword_offer/src/Offer03.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 题目link: https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+ * 描述: 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ *
+ * <p>
+ * 解法: 遍历数组，每得到一个数就在对应下标的数*-1，那么负数就表明该下标出现过，也就是说遇到负数就等于重复了，0的情况特判
+ */
+public class Offer03 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 3, 1, 0, 2, 5, 3};
+        System.out.println(findRepeatNumber(nums));
+    }
+
+    public static int findRepeatNumber(int[] nums) {
+        boolean[] isVisited = new boolean[nums.length];
+        for (int num : nums) {
+            if (isVisited[num]) {
+                return num;
+            } else {
+                isVisited[num] = true;
+            }
+        }
+        return 0;
+    }
+}
